45e5d37c97f0941c03d4c163cc13652c5764c1db
wkwksama <mailpakgasing@gmail.com>
2019-03-26 16:08:56 +0700

    lib list chapters

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const Telegraf = require('telegraf')
 const mangakita = require('./lib/mangakita')
 
-const ONE_PIECE_URL = 'https://mangakita.net/one-piece-chapter-937-bahasa-indonesia/'
+const ONE_PIECE_URL = 'https://mangakita.net/one-piece-chapter-843/'
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-(async function() {
-  const onepiece = await mangakita.extract(ONE_PIECE_URL)
-  console.log(onepiece)
-})();
+(async function () {
+  const chapters = await mangakita.extractImages(ONE_PIECE_URL)
+  console.log(chapters)
+})()
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
