ae7a0b6e9b1e97c4faf36536b5e4c29f64629a12
wkwksama <mailpakgasing@gmail.com>
2019-03-26 14:33:05 +0700

    ekstrak halaman manga jadi array gambar

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
 const Telegraf = require('telegraf')
+const mangakita = require('./lib/mangakita')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const ONE_PIECE_URL = 'https://mangakita.net/one-piece-chapter-937-bahasa-indonesia/'
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+(async function() {
+  const onepiece = await mangakita.extract(ONE_PIECE_URL)
+  console.log(onepiece)
+})();
+
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
